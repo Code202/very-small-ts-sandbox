7be97946aa52e6ebc997661cd7b1e966b7288a94
scalpaine <ybahou.pro@gmail.com>
2021-04-30 13:19:18 +0200

    final version

--- src/bootstrap.test.ts
+++ src/bootstrap.test.ts
@@ -1,5 +1,14 @@
-import {fooBarQix} from './bootstrap';
+import { fooBarQix } from "./bootstrap";
 
-it('should run', function () {
-    expect(fooBarQix(1)).toEqual(1);
+it("should run", function () {
+  expect(fooBarQix(3)).toEqual("FooFoo");
+  expect(fooBarQix(5)).toEqual("BarBar");
+  expect(fooBarQix(7)).toEqual("QixQix");
+});
+
+it("should run whit different digits", function () {
+  expect(fooBarQix(15)).toEqual("FooBarBar");
+  expect(fooBarQix(30)).toEqual("FooBarFoo*");
+  expect(fooBarQix(35)).toEqual("BarQixFooBar");
+  expect(fooBarQix(4)).toEqual("4");
 });
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,3 +1,32 @@
-export function fooBarQix(number: number) {
-    return number;
+export function fooBarQix(number: number): string {
+  let result = "";
+  const ref = number.toString();
+
+  if (number % 3 === 0) {
+    result = "Foo";
+  }
+  if (number % 5 === 0) {
+    result += "Bar";
+  }
+  if (number % 7 === 0) {
+    result += "Qix";
+  }
+
+  if (!result.length) {
+    result = number.toString();
+  }
+
+  for (let i = 0; i < ref.length; i++) {
+    if (ref[i] === "0") {
+      result += "*";
+    } else if (ref[i] === "3") {
+      result += "Foo";
+    } else if (ref[i] === "5") {
+      result += "Bar";
+    } else if (ref[i] === "7") {
+      result += "Qix";
+    }
+  }
+
+  return result;
 }
